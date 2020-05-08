3bdea35d8ded5c7741aaa491232aaf07f57a5f79
Kevin <hartyanyi.kevin@gmail.com>
2020-05-09 00:12:52 +0200

    Started working on SignInManager

--- Assignment2_API/Assignment_WebShop_2_1/WebAPI/Controllers/AccountController.cs
+++ Assignment2_API/Assignment_WebShop_2_1/WebAPI/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using Data.Models;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
